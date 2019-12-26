471526f6913165dfdd342340b519591d04e10b31
中里南 <minami.rage@gmail.com>
2019-12-26 12:50:16 +0900

    fix : ルーティング修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:image,:profile])
     end
-  end
-
 end
