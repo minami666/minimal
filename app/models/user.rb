f8b005f5a14ef369bd07f6c233f51b4aa269cccf
中里南 <minami.rage@gmail.com>
2019-12-27 21:07:08 +0900

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
 
   has_many :roomimages,dependent: :destroy
   has_many :items,dependent: :destroy
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
