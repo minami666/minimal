24c9a9be5ad3e9e4f89588c43f45ef84e2a42ce4
中里南 <minami.rage@gmail.com>
2019-12-27 20:16:33 +0900

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
 
-  has_many :roomimages
+  has_many :roomimages,dependent: :destroy
+  has_many :items,dependent: :destroy
 end
