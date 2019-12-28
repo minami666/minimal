2d433dc324f9be15e0243958fdf89b8ed1ea69ca
中里南 <minami.rage@gmail.com>
2019-12-29 06:22:51 +0900

    add : roomlikesテーブル

--- db/migrate/20191228204209_create_roomlikes.rb
+++ db/migrate/20191228204209_create_roomlikes.rb
@@ -1,7 +1,8 @@
 class CreateRoomlikes < ActiveRecord::Migration[5.2]
   def change
     create_table :roomlikes do |t|
-
+      t.references :user
+      t.references :roomimage
       t.timestamps
     end
   end
