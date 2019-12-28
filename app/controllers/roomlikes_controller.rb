b7c6b80b57f305b9e51d2b814af90f666b8a3d7f
中里南 <minami.rage@gmail.com>
2019-12-29 05:54:55 +0900

    add : roomlikesコントローラ

--- app/controllers/roomlikes_controller.rb
+++ app/controllers/roomlikes_controller.rb
@@ -1,2 +1,14 @@
 class RoomlikesController < ApplicationController
+
+  def create
+    @roomlike = current_user.roomlikes.create(post_id: params[:post_id])
+    redirect_to root_path
+  end
+
+  def destroy
+    @roomlike = Roomlike.find_by(roomimages_id: params[:roomimages_id], user_id: current_user.id)
+    @roomlike.destroy
+    redirect_to root_path
+  end
+
 end
