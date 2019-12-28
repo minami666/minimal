ed6a36bb29381b19a611c21185b195340bd387df
中里南 <minami.rage@gmail.com>
2019-12-29 03:41:08 +0900

    add : gem

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,13 @@ module Minimal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.generators.system_tests = nil
 
     config.generators do |g|
-      g.javascripts false
+      g.assets false
       g.helper false
       g.test_framework :rspec,
         view_specs: false,
@@ -31,17 +35,5 @@ module Minimal
         controller_specs: false,
         routing_specs: false
     end
-
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-    config.i18n.default_locale = :ja
   end
 end
